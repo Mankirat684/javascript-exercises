54f35bad02b7bf3008305d543fdb31d0e2a4aaee
Mankirat684 <mankirats213@gmail.com>
2024-08-13 13:16:30 +0530

    Update palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  for (i=0;i<string.length;i++){
+    if (string(i) === string(string.length-i)){
+      console.log("string is a palindrome")}
+  }else{
+    console.log("string is not a palindrome")
+  }
 };
 
 // Do not edit below this line
