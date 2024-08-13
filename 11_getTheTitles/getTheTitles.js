83c6268b0a3e7058115138c73ba274b34d5c8b57
Mankirat684 <mankirats213@gmail.com>
2024-08-13 14:13:44 +0530

    Update getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
-
-};
+function getTheTitles(Array){
+    let arr=[]
+    for(i=0;i<Array.length;i++){
+       arr[i] = Array[i][`title`]
+    }
+console.log(arr)
+}
+getTheTitles(books)
 
 // Do not edit below this line
 module.exports = getTheTitles;
